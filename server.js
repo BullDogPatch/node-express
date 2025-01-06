1064965c145bdee85adb8d88b7635ac586c8f4b1
Craig Clayton <craig__clayton@outlook.com>
2025-01-06 15:55:40 +0000

    got basic post working

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.listen(8080, () => console.log('My server is up and running.'));
+
+app.get('/', (request, response) => {
+  response.send('<h1>This is from the server</h1>');
+});
+
+app.get('/madagasca', (request, response) => {
+  response.json({ message: 'Hi Mom, I am somewhere in Africa' });
+});
+
+app.get('/secret-post', (request, response) => {
+  response.json({ message: 'This is super secret' });
+});
+
+app.post('/secret-post', (request, response) => {
+  const data = request.body;
+  response.json(data);
+});
+
+// app.post('/add-new-data', function (request, response) {
+//   //The client request will contain the data body
+//   const data = request.body;
+//   //Some code to add this data body into the database
+//   response.json(data);
+// });
